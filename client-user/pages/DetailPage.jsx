9912c796ca828ff5270e03823a4bb87f83b1a0f5
HuseinHQ <mhusein463@gmail.com>
2023-11-08 07:39:00 +0700

    Update detail page

--- client-user/pages/DetailPage.jsx
+++ client-user/pages/DetailPage.jsx
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/core";
 import { useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 
 export default function DetailPage() {
   const route = useRoute();
@@ -24,9 +24,42 @@ export default function DetailPage() {
     }
   };
 
+  function formatDate() {
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    const theDate = new Date(data.createdAt);
+    const formattedDate = theDate.toLocaleDateString("id-ID", options);
+    return formattedDate;
+  }
+
   return (
-    <View className="bg-white">
-      <Image source={{ uri: data.Company?.companyLogo }} className="w-20 h-20" />
-    </View>
+    <ScrollView className="gap-2">
+      <View className="bg-white pb-3 px-2 gap-3">
+        <View className="pl-5">
+          <Image source={{ uri: data.Company?.companyLogo }} className="w-10 h-10 " />
+        </View>
+        <View>
+          <Text className="text-slate-800 text-lg font-bold">{data.title}</Text>
+          <Text className="text-slate-500">{data.Company?.name}</Text>
+        </View>
+        <View>
+          <Text className="text-slate-800 font-bold">{data.Company?.location}</Text>
+        </View>
+        <View>
+          <Text className="text-slate-400 text-xs">Ditayangkan pada {formatDate()}</Text>
+        </View>
+      </View>
+
+      <View className="bg-white p-4">
+        <View>
+          <Text className="font-bold text-slate-800 text-base">Deskripsi Pekerjaan</Text>
+          <Text className="text-slate-800 text-sm mt-2">Tanggung Jawab Utama :</Text>
+          <View className="grid grid-cols-10 gap-2">
+            <Text className="text-slate-800 font-extrabold text-lg col-span-1">{"\u2022"}</Text>
+            <Text className="text-slate-800 text-sm col-span-9">{data.description}</Text>
+          </View>
+          <Text className="font-bold text-slate-800 text-base">Deskripsi Pekerjaan</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
