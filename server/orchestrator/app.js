87abc7dc2689be32de0b4886012d626fdb277384
HuseinHQ <mhusein463@gmail.com>
2023-11-13 22:53:31 +0700

    cors

--- server/orchestrator/app.js
+++ server/orchestrator/app.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+const cors = require("cors");
+
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { appTypeDefs, appResolvers } = require("./schema/app");
@@ -10,6 +12,10 @@ const server = new ApolloServer({
   typeDefs: [appTypeDefs, userTypeDefs],
   resolvers: [appResolvers, userResolvers],
   instropection: true,
+});
+
+server.applyMiddLeware({
+  cors: true,
 });
 
 startStandaloneServer(server, {
